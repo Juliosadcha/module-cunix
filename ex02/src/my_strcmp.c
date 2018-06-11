c47338b1c973b3119ec14b8de60711fb432a4448
juli <juli_o@i.ua>
2018-06-11 16:52:38 +0300

    Ex02 is finished

---
+++ ex02/src/my_strcmp.c
@@ -0,0 +1,26 @@
+#include "test.h"
+
+int my_strcmp(char *s1, char *s2)
+{
+  int flag = 0;
+
+  while(*s1 != '\0' || *s2 != '\0'){
+    if(*s1 > *s2) {
+      flag = 1;
+      break;
+    }
+    else {
+      if(*s1 < *s2) {
+        flag = -1;
+        break;
+      }
+      else {
+        flag = 0;
+      }
+    }
+    s1++;
+    s2++;
+  }
+
+  return flag;
+}
