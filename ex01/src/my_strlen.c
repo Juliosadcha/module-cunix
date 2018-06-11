bf343764156c3701e2c9e4ef1aa05bc05ae9e7a9
juli <juli_o@i.ua>
2018-06-11 16:44:01 +0300

    Ex01 is finished

---
+++ ex01/src/my_strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "test.h"
+
+unsigned int my_strlen(char *str)
+{
+  size_t i;
+
+  for (i = 0; str[i]; i++);
+  return i;
+}
+
