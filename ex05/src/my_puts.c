da66b52a3ce9958bb1234004685d9268778fa8bf
juli <juli_o@i.ua>
2018-06-14 11:10:31 +0300

    Ex05 is finished

---
+++ ex05/src/my_puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "test.h"
+
+int my_puts(const char* s)
+{
+    int cnt = 0;
+    while(s[cnt])
+    {
+        if (putchar(s[cnt]) == EOF)
+            return EOF;
+        cnt++;
+    }
+    if (putchar('\n') == EOF)
+        return EOF;
+    return 1;
+}
