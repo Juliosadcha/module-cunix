76e63f9ea6eee922d6f55deb80922776a3689088
juli <juli_o@i.ua>
2018-06-18 16:30:27 +0300

    Fix Ex03

--- ex03/src/my_strcpy.c
+++ ex03/src/my_strcpy.c
@@ -1,18 +1,18 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <string.h>
 #include "test.h"
+
 char *my_strcpy(char *dest, const char *src)
 {
-  size_t i = 0;
+  size_t i;
+  i = 0;
 
-  for (i; src[i]!= '\0'; i++)
-  {
+  for (i; src[i]!= '\0'; i++){
     dest[i] = src[i];
   }
+
   dest[i] = '\0';
+
   return dest;
 }
 
