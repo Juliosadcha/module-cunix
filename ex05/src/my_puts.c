71cf6bb8e9fc1ca60dc06ff58d4da3ecb9a21081
juli <juli_o@i.ua>
2018-06-18 16:30:56 +0300

    Fix Ex05

--- ex05/src/my_puts.c
+++ ex05/src/my_puts.c
@@ -3,14 +3,17 @@
 
 int my_puts(const char* s)
 {
-    int cnt = 0;
+    int cnt;
+    cnt = 0;
     while(s[cnt])
     {
         if (putchar(s[cnt]) == EOF)
             return EOF;
         cnt++;
     }
+
     if (putchar('\n') == EOF)
         return EOF;
+
     return 1;
 }
