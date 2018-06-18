4f72844f268bcea666b2f45691668f2d160fd1ff
juli <juli_o@i.ua>
2018-06-18 16:30:39 +0300

    Fix Ex04

--- ex04/src/my_atoi.c
+++ ex04/src/my_atoi.c
@@ -11,25 +11,29 @@ int is_num(char s)
 
 int my_atoi(const char *str)
 {
-  int sign = 0;
-  int result = 0;
+  int sign, result;
+  sign = 0;
+  result = 0;
   if(*str == '-') {
     sign = 1;
   } else {
     sign = 0;
   }
+
   if (!str)
     return 0;
-  for (int i = sign; str[i] != '\0'; ++i)
-  {
+
+  for (int i = sign; str[i] != '\0'; ++i){
     if(is_num(str[i]) == 1)
       result = result*10 + str[i] - '0';
     else
       break;
   }
-  if(sign == 1) {
+
+  if (sign == 1) {
     result = -1 * result;
   }
+
   return result;
 }
 
--- ex04/src/my_itoa.c
+++ ex04/src/my_itoa.c
@@ -1,34 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "test.h"
-size_t intlen(int n){
+
+size_t intlen(int n)
+{
     if (!n){
         return 1;
     }
-    int len = 0;
+
+    int len;
+    len = 0;
     while (n){
         ++len;
         n /= 10;
     }
+
     return len;
 }
 
-char* my_itoa(int n){
-    int neg = (n < 0);
+char* my_itoa(int n)
+{
+    int neg, i, nlen;
+    neg = (n < 0);
     if (neg){
         n = -n;
     }
-    size_t nlen = intlen(n);
+
+    nlen = intlen(n);
     nlen += neg;
     char *cstr = malloc(nlen + 1);
     if (neg){
         cstr[0] = '-';
     }
+
     cstr[nlen] = '\0';
-    int i;
     for (i = nlen - 1; i >= 0 + neg; --i){
         cstr[i] = ((n % 10) + '0');
         n /= 10;
     }
+
     return cstr;
 }
+
